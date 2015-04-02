0906e4142a859174772950e88821d26ceead3801
Andy Au <andyau@dhcp-128-189-232-113.ubcsecure.wireless.ubc.ca>
2015-04-02 15:40:56 -0700

    initial

---
+++ server.js
@@ -0,0 +1,26 @@
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , Firebase = require('firebase')  
+
+var app = express()
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
